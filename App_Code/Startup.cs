b3947bd6394d8d3617820ae9323e14838b6a3e22
Jacob Schlecht <jacob.lee.schlecht@gmail.com>
2017-04-19 19:38:15 -0500

    Initial website creation. Should contain access to the database but it may not.

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Merge.Startup))]
+namespace Merge
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
