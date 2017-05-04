2637d5eb0cade4094c2b99f9f333f7fe05e5577a
Ben Myers <ben.myers@okstate.edu>
2017-05-03 21:55:54 -0500

    Tabs in the modal!

--- Scripts/modal.js
+++ Scripts/modal.js
@@ -1,1 +1,39 @@
-﻿
+﻿$(document).ready(function () {
+	$(document).delegate('#WriteTextBox', 'keydown', function (e) {
+		var keyCode = e.keyCode || e.which;
+
+		if (keyCode == 9) {
+			e.preventDefault();
+			var start = this.selectionStart;
+			var end = this.selectionEnd;
+
+			// set textarea value to: text before caret + tab + text after caret
+			$(this).val($(this).val().substring(0, start)
+				+ "\t"
+				+ $(this).val().substring(end));
+
+			// put caret at right position again
+			$(this).get(0).selectionStart =
+				$(this).get(0).selectionEnd = start + 1;
+		}
+	});
+
+	$(document).delegate('#WriteCodeBox', 'keydown', function (e) {
+		var keyCode = e.keyCode || e.which;
+
+		if (keyCode == 9) {
+			e.preventDefault();
+			var start = this.selectionStart;
+			var end = this.selectionEnd;
+
+			// set textarea value to: text before caret + tab + text after caret
+			$(this).val($(this).val().substring(0, start)
+				+ "\t"
+				+ $(this).val().substring(end));
+
+			// put caret at right position again
+			$(this).get(0).selectionStart =
+				$(this).get(0).selectionEnd = start + 1;
+		}
+	});
+});
